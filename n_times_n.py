02d1a868bd3fecb2c74ad9232300d2598d47a26c
tubiky <61419018+tubiky@users.noreply.github.com>
2020-02-24 23:18:33 +0900

    Update n_times_n.py

--- n_times_n.py
+++ n_times_n.py
@@ -11,8 +11,8 @@ name = input("What is your Name? ")
 # streak = pass
 
 def intro():
-    print(" 구구단을 연습해 봅시다.")
-    print(name + "님 안녕하세요. 지금부터 구구단을 연습하겠습니다. ")
+    print(" Let's Practice N times N!")
+    print("Hello" + name+"!" + "Welcome")
     print("--------" * 7)
 
 def stopwatch(seconds):
@@ -33,23 +33,23 @@ while game_running == True:
     answer = rand_number1 * rand_number2
 
     print(str(rand_number1) + " X " + str(rand_number2))
-    user_answer = input(" 정답은 ? ")
+    user_answer = input(" What's Your Answer? ")
     # stopwatch(4)
     if user_answer.isdigit() == True:
         if int(user_answer) == answer:
             answer_count += 1
-            print(" Wow!!! 정답입니다! " + name + "님")
+            print(" Wow!!! Correct!! " + name)
 
         elif int(user_answer) != answer or user_answer == None:
-            print(" Oops!!! 오답입니다! " + name + "님")
+            print(" Oops!!! Wrong!! " + name)
             wrong_answer.append([rand_number1, rand_number2])
             print(wrong_answer)
 
             if len(wrong_answer) > 9:
-                print(" 10문제를 틀리셨습니다. 복습모드로 들어가시겠습니까? ( Y / N ) ")
+                print(" You Got 10 Questions Wrong. Do you wanna review them? ( Y / N ) ")
                 user_choice = input("")
                 if user_choice == "y" or user_choice == "Y":
-                    print(" 복습모드로 들어갑니다. ")
+                    print(" Review mode has been activated!! ")
                     reveiw = True
                     if reveiw == True:
                         for i in wrong_answer:
@@ -57,17 +57,17 @@ while game_running == True:
                             rand_number2 = i[1]
                             answer = int(rand_number1) * int(rand_number2)
                             print(rand_number1, " X ", rand_number2, " = ")
-                            user_answer = input(print(" 정답은 ? "))
+                            user_answer = input(print(" What's Your Answer? "))
                             if user_answer.isspace() or user_answer.isalpha():
                                 print(" 숫자만 입력가능합니다. ")
                             if user_answer.isdigit():
                                 if int(user_answer) == answer:
-                                    print(" 정답입니다. ^_^ ")
+                                    print(" Correct!! ^_^ ")
                                 else:
-                                    print(" 두 번 틀렸습니다. T_T ")
+                                    print(" You Got This Wrong 2 Times! T_T ")
                 if user_choice == "n" or user_choice == "N":
                     if answer_count != count:
-                        print(" 당신의 점수는 {} / {} 입니다. 수고하셨습니다.".format(answer_count, count))
+                        print(" Your final SCORE is {} / {}. Well Done.".format(answer_count, count))
                         game_running = False
                 elif user_choice.isdigit() or user_choice.isspace():
                     print(" y 또는 n을 입력해주세요. ")
